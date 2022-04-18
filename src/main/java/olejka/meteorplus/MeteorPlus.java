15c7820677826b0d972df6f343e6c524b8d98141
Vazgen005 <norvardyanvazgen@gmail.com>
2022-04-19 01:07:36 +0300

    Add Auto Leave + icon
    Automatically logs out from the server when someone enters your render distance.
    Also added the icon (emerald block).
    Fixed critical template typo (thx void ❤️)

--- src/main/java/olejka/meteorplus/MeteorPlus.java
+++ src/main/java/olejka/meteorplus/MeteorPlus.java
@@ -8,6 +8,8 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 //import meteordevelopment.meteorclient.systems.commands.Commands;
 
+import net.minecraft.item.Items;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,17 +17,18 @@ import java.lang.invoke.MethodHandles;
 
 public class MeteorPlus extends MeteorAddon {
 	public static final Logger LOG = LogManager.getLogger();
-	public static final Category CATEGORY = new Category("Meteor Plus");
+	public static final Category CATEGORY = new Category("Meteor Plus", Items.EMERALD_BLOCK.getDefaultStack());
 
 	@Override
 	public void onInitialize() {
 		LOG.info("Initializing MeteorPlus");
 
 		// Required when using @EventHandler
-		MeteorClient.EVENT_BUS.registerLambdaFactory("meteordevelopment.addons.meteorplus", (lookupInMethod, klass) -> (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
+		MeteorClient.EVENT_BUS.registerLambdaFactory("olejka.meteorplus", (lookupInMethod, klass) -> (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
 
 		//Modules
 		Modules.get().add(new AntiXRayBypass());
+		Modules.get().add(new AutoLeave());
 
 		//Commands
 		//Commands.get().add();
---
+++ src/main/java/olejka/meteorplus/modules/AutoLeave.java
@@ -0,0 +1,52 @@
+package olejka.meteorplus.modules;
+
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.friends.Friends;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
+import net.minecraft.text.LiteralText;
+import olejka.meteorplus.MeteorPlus;
+import meteordevelopment.meteorclient.events.entity.EntityAddedEvent;
+import meteordevelopment.orbit.EventHandler;
+public class AutoLeave extends Module {
+	public AutoLeave() {
+		super(MeteorPlus.CATEGORY, "Auto Leave", "Automatically logs out from the server when someone enters your render distance.");
+	}
+	private final SettingGroup ALSettings = settings.createGroup("Visual Range");
+	private final Setting<Boolean> visualRangeIgnoreFriends = ALSettings.add(new BoolSetting.Builder()
+		.name("ignore-friends")
+		.description("Ignores friends.")
+		.defaultValue(true)
+		.build()
+	);
+
+	private final Setting<Boolean> AutoDisable = ALSettings.add(new BoolSetting.Builder()
+		.name("auto-disable")
+		.description("Disables function after player detect.")
+		.defaultValue(true)
+		.build()
+	);
+
+	@EventHandler
+	public void onEntityAdded(EntityAddedEvent event) {
+		if (visualRangeIgnoreFriends.get()) {
+			if (event.entity.isPlayer() && !Friends.get().isFriend(((PlayerEntity) event.entity))) {
+				assert mc.player != null;
+				mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(new LiteralText (String.format("[Auto Leaeve] player %s was detected", event.entity.getEntityName()))));
+				if (AutoDisable.get()) this.toggle();
+			}
+		}
+		else {
+			if (event.entity.isPlayer()) {
+				assert mc.player != null;
+				mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(new LiteralText (String.format("[Auto Leaeve] player %s was detected", event.entity.getEntityName()))));
+				if (AutoDisable.get()) this.toggle();
+			}
+		}
+	}
+}
+
+
