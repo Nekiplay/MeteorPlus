dc17cf40fbb1252e95bcb669a30e483cd6876fdf
Vazgen005 <norvardyanvazgen@gmail.com>
2022-04-30 23:24:22 +0300

    Added Freeze -> Freeze look

--- src/main/java/olejka/meteorplus/modules/Freeze.java
+++ src/main/java/olejka/meteorplus/modules/Freeze.java
@@ -1,6 +1,10 @@
 package olejka.meteorplus.modules;
 
+import meteordevelopment.meteorclient.events.meteor.ActiveModulesChangedEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
@@ -10,10 +14,33 @@ public class Freeze extends Module {
 	public Freeze() {
 		super(MeteorPlus.CATEGORY, "Freeze", "Freezes your position.");
 	}
+	private final SettingGroup FSettings = settings.createGroup("Freeze Settings");
+
+	private final Setting<Boolean> FreezeLook = FSettings.add(new BoolSetting.Builder()
+		.name("Freeze look")
+		.description("Freezes your pitch and yaw.")
+		.defaultValue(false)
+		.build()
+	);
+
+	@Override()
+	public void onActivate() {
+		if (mc.player != null){
+			yaw = mc.player.getYaw();
+			pitch = mc.player.getPitch();
+		}
+	}
 
 	@EventHandler(priority = EventPriority.HIGH)
 	private void onTick(TickEvent.Pre event) {
 		if (mc.player == null) return;
 		mc.player.setVelocity(0, 0, 0);
+		if (FreezeLook.get()) {
+			mc.player.setYaw(yaw);
+			mc.player.setPitch(pitch);
+		}
 	}
+
+	float yaw = 0;
+	float pitch = 0;
 }
