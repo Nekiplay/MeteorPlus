dcf95b698ff6f3972c74d8daf6785f4dc6448748
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2022-05-02 08:16:02 +0500

    Matrix Jesus

--- src/main/java/olejka/meteorplus/modules/AntiLava.java
+++ src/main/java/olejka/meteorplus/modules/AntiLava.java
@@ -1,6 +1,7 @@
 package olejka.meteorplus.modules;
 
 import meteordevelopment.meteorclient.events.entity.player.BreakBlockEvent;
+import meteordevelopment.meteorclient.events.entity.player.PlayerMoveEvent;
 import meteordevelopment.meteorclient.events.entity.player.StartBreakingBlockEvent;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
@@ -10,13 +11,16 @@ import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.entity.EntityUtils;
 import meteordevelopment.meteorclient.utils.misc.Vec3;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.MovementType;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.Hand;
@@ -26,9 +30,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import olejka.meteorplus.MeteorPlus;
+import olejka.meteorplus.utils.BlockHelper;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class AntiLava extends Module {
 	public AntiLava() {
@@ -40,14 +46,22 @@ public class AntiLava extends Module {
 	public final Setting<Boolean> replaceLava = ALSettings.add(new BoolSetting.Builder()
 		.name("Replace lava")
 		.description("Place blocks in lava in offhand.")
-		.defaultValue(false)
+		.defaultValue(true)
+		.build()
+	);
+
+	public final Setting<Boolean> moveReplaceLava = ALSettings.add(new BoolSetting.Builder()
+		.name("Move replace lava")
+		.description("Replace lava in movement.")
+		.defaultValue(true)
+		.visible(replaceLava::get)
 		.build()
 	);
 
 	private final Setting<Integer> delay = ALSettings.add(new IntSetting.Builder()
 		.name("Repalce delay")
 		.description("Delay for replace lava.")
-		.defaultValue(3)
+		.defaultValue(0)
 		.min(0)
 		.visible(replaceLava::get)
 		.sliderRange(0, 20)
@@ -73,6 +87,7 @@ public class AntiLava extends Module {
 	ArrayList<BlockPos> lava = new ArrayList<BlockPos>();
 
 	private Integer tick = 0;
+
 	@EventHandler
 	private void onTickEvent(TickEvent.Post event) {
 		if (mc.crosshairTarget != null && mc.crosshairTarget.getType() == HitResult.Type.BLOCK) {
@@ -91,6 +106,16 @@ public class AntiLava extends Module {
 						else
 						{
 							tick--;
+						}
+					}
+					else if (moveReplaceLava.get())
+					{
+						BlockPos under = mc.player.getBlockPos().add(0, -1, 0);
+						List<BlockPos> blocks = getBlocks(under, 2, 0);
+						for (BlockPos pos : blocks) {
+							if (mc.world.getBlockState(pos).getMaterial() == Material.LAVA) {
+								lava.add(pos);
+							}
 						}
 					}
 				}
@@ -113,10 +138,29 @@ public class AntiLava extends Module {
 		}
 	}
 
+	private List<BlockPos> getBlocks(BlockPos startPos, int y_radius, int radius)
+	{
+		List<BlockPos> temp = new ArrayList<>();
+		for (int dy = -y_radius; dy <= y_radius; dy++) {
+			for (int dz = -radius; dz <= radius; dz++) {
+				for (int dx = -radius; dx <= radius; dx++) {
+					BlockPos blockPos = new BlockPos(startPos.getX() + dx, startPos.getY() + dy, startPos.getZ() + dz);
+					if (EntityUtils.isInRenderDistance(blockPos)) {
+						temp.add(blockPos);
+					}
+				}
+			}
+		}
+		return temp;
+	}
+
 	private ArrayList<BlockPos> isExposedLava(BlockPos pos)
 	{
 		ArrayList<BlockPos> blocks = new ArrayList<>();
 		if (mc.world != null) {
+			if (mc.world.getBlockState(pos).getMaterial() == Material.LAVA) {
+				blocks.add(pos);
+			}
 			if (mc.world.getBlockState(pos.add(1, 0, 0)).getMaterial() == Material.LAVA) {
 				blocks.add(pos.add(1, 0, 0));
 			}
---
+++ src/main/java/olejka/meteorplus/modules/JesusPlus.java
@@ -0,0 +1,70 @@
+package olejka.meteorplus.modules;
+
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.mixininterface.IVec3d;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import olejka.meteorplus.MeteorPlus;
+
+public class JesusPlus extends Module {
+	public JesusPlus() {
+		super(MeteorPlus.CATEGORY, "Jesus Plus", "Matrix Jesus.");
+	}
+
+	private final SettingGroup ALSettings = settings.createGroup("Auto Leave Settings");
+	private final Setting<Double> speed = ALSettings.add(new DoubleSetting.Builder()
+		.name("Speed")
+		.description("Rescan delay.")
+		.defaultValue(1.25)
+		.max(2500)
+		.sliderRange(0, 2500)
+		.build()
+	);
+	private final Float range = 0.005f;
+	@Override
+	public void onActivate() {
+
+	}
+
+	@EventHandler
+	private void onTick(TickEvent.Post event) {
+		float yaw = mc.player.getYaw();
+		Vec3d forward = Vec3d.fromPolar(0, yaw);
+		Vec3d right = Vec3d.fromPolar(0, yaw + 90);
+		double velX = 0;
+		double velZ = 0;
+
+		double s = 0.5;
+
+		double speedValue = speed.get();
+
+
+		if (mc.options.forwardKey.isPressed()) {
+			velX += forward.x * s * speedValue;
+			velZ += forward.z * s * speedValue;
+		}
+		if (mc.options.backKey.isPressed()) {
+			velX -= forward.x * s * speedValue;
+			velZ -= forward.z * s * speedValue;
+		}
+
+		if (mc.options.rightKey.isPressed()) {
+			velX += right.x * s * speedValue;
+			velZ += right.z * s * speedValue;
+		}
+		if (mc.options.leftKey.isPressed()) {
+			velX -= right.x * s * speedValue;
+			velZ -= right.z * s * speedValue;
+		}
+		if (mc.world.getBlockState(new BlockPos(mc.player.getPos().x, mc.player.getPos().y + range, mc.player.getPos().z)).getBlock() == Blocks.WATER && !mc.player.horizontalCollision) {
+			((IVec3d) mc.player.getVelocity()).set(velX, 0, velZ);
+		}
+	}
+
+}
