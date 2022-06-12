18c32e26057823ea997518e948cb01bb05b4c225
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2022-06-12 06:15:13 +0500

    Vulcan Speed Removed warning

--- src/main/java/olejka/meteorplus/modules/speed/modes/Vulcan.java
+++ src/main/java/olejka/meteorplus/modules/speed/modes/Vulcan.java
@@ -17,6 +17,8 @@ import olejka.meteorplus.modules.speed.SpeedMode;
 import olejka.meteorplus.modules.speed.SpeedModes;
 import olejka.meteorplus.utils.CustomSpeedUtils;
 
+import java.util.Objects;
+
 public class Vulcan extends SpeedMode {
 	public Vulcan() {
 		super(SpeedModes.Vulcan);
@@ -45,28 +47,18 @@ public class Vulcan extends SpeedMode {
 				}
 			}
 		}
-		if (mc.player.hasStatusEffect(StatusEffects.SPEED)) {
-			int amplifier = mc.player.getStatusEffect(StatusEffects.SPEED).getAmplifier();
-			if (amplifier >= 1) {
-
-			}
-			else {
-				settings.warning("Vulcan speed need speed effect 2");
-			}
-		}
-		else {
-			settings.warning("Vulcan speed need speed effect 2");
-		}
 	}
 
 	@Override
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
-		if (mc.player.getEquippedStack(EquipmentSlot.CHEST).getItem() == Items.ELYTRA) {
-			if (mc.player.hasStatusEffect(StatusEffects.SPEED) && mc.player.getStatusEffect(StatusEffects.SPEED).getAmplifier() == 1) {
-				CustomSpeedUtils.applySpeed(event, settings.speedVulcanef2.get());
-			}
-			else if (mc.player.hasStatusEffect(StatusEffects.SPEED) && mc.player.getStatusEffect(StatusEffects.SPEED).getAmplifier() == 0) {
-				CustomSpeedUtils.applySpeed(event, settings.speedVulcanef1.get());
+		if (mc.player != null && mc.player.getEquippedStack(EquipmentSlot.CHEST).getItem() == Items.ELYTRA) {
+			if (mc.player.hasStatusEffect(StatusEffects.SPEED) && mc.player.getStatusEffect(StatusEffects.SPEED) != null) {
+				if (Objects.requireNonNull(mc.player.getStatusEffect(StatusEffects.SPEED)).getAmplifier() == 1) {
+					CustomSpeedUtils.applySpeed(event, settings.speedVulcanef2.get());
+				}
+				else if (Objects.requireNonNull(mc.player.getStatusEffect(StatusEffects.SPEED)).getAmplifier() == 0) {
+					CustomSpeedUtils.applySpeed(event, settings.speedVulcanef1.get());
+				}
 			}
 			else {
 				CustomSpeedUtils.applySpeed(event, settings.speedVulcanef0.get());
