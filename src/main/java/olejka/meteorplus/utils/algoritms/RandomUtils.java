498b78b6420d1ba1b6fa66910fcd343be6c31144
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2022-07-04 07:10:13 +0500

    Fix killaura plus

--- src/main/java/olejka/meteorplus/utils/algoritms/RandomUtils.java
+++ src/main/java/olejka/meteorplus/utils/algoritms/RandomUtils.java
@@ -4,15 +4,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
 	public static float nextFloat(float min, float max) {
-		if (min == max) return max;
-		return ThreadLocalRandom.current().nextFloat(min, max);
+		if (max <= min) return max;
+		else return ThreadLocalRandom.current().nextFloat(min, max);
 	}
 	public static int nextInt(int min, int max) {
-		if (min == max) return max;
-		return ThreadLocalRandom.current().nextInt(min, max);
+		if (max <= min) return max;
+		else return ThreadLocalRandom.current().nextInt(min, max);
 	}
 	public static double nextDouble(double min, double max) {
-		if (min == max) return max;
-		return ThreadLocalRandom.current().nextDouble(min, max);
+		if (max <= min) return max;
+		else return ThreadLocalRandom.current().nextDouble(min, max);
 	}
 }
