fc744b928ce9ef999271d0a86389bc09aff29e89
Vazgen005 <norvardyanvazgen@gmail.com>
2022-07-08 07:07:10 +0300

    help someone

--- src/main/java/olejka/meteorplus/hud/MeteorPlusLogoHud.java
+++ src/main/java/olejka/meteorplus/hud/MeteorPlusLogoHud.java
@@ -1,8 +1,7 @@
 package olejka.meteorplus.hud;
 
+import meteordevelopment.meteorclient.utils.render.color.Color;
 import olejka.meteorplus.MeteorPlus;
-import meteordevelopment.meteorclient.renderer.GL;
-import meteordevelopment.meteorclient.renderer.Renderer2D;
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
 import meteordevelopment.meteorclient.settings.Setting;
@@ -12,12 +11,9 @@ import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import net.minecraft.util.Identifier;
 
-import static meteordevelopment.meteorclient.utils.Utils.WHITE;
-
-
 public class MeteorPlusLogoHud extends HudElement {
 
-	public static final HudElementInfo<MeteorPlusLogoHud> INFO = new HudElementInfo<>(MeteorPlus.HUD_GROUP, "MeteorPlusLogo", "Shows the Meteor Plus logo in the HUD.", MeteorPlusLogoHud::new);
+	public static final HudElementInfo<MeteorPlusLogoHud> INFO = new HudElementInfo<>(MeteorPlus.HUD_GROUP, "MeteorPlusLogo", "Shows the MeteorPlus logo in the HUD.", MeteorPlusLogoHud::new);
 	private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
 	private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
@@ -25,6 +21,7 @@ public class MeteorPlusLogoHud extends HudElement {
 		.description("The scale of the logo.")
 		.defaultValue(3)
 		.min(0.1)
+		.onChanged((size) -> calculateSize())
 		.sliderRange(0.1, 10)
 		.build()
 	);
@@ -50,13 +47,13 @@ public class MeteorPlusLogoHud extends HudElement {
 
 	@Override
 	public void render(HudRenderer renderer) {
-		//GL.bindTexture(TEXTURE);
-		//renderer.begin();
+		int w = getWidth();
+		int h = getHeight();
+
 		if (!invert.get()) {
-			renderer.texture(TEXTURE, box.getRenderX(), box.getRenderY(), 64 * scale.get(), 50 * scale.get(), WHITE);
+			renderer.texture(TEXTURE, box.getRenderX(), box.getRenderY(), w, h, Color.WHITE);
 		} else {
-			renderer.texture(TEXTURE, box.getRenderX()+(64 * scale.get()), box.getRenderY(), -(64 * scale.get()), 50 * scale.get(), WHITE);
+			renderer.texture(TEXTURE, box.getRenderX()+w, box.getRenderY(), -w, h, Color.WHITE);
 		}
-		//renderer.end();
 	}
 }
