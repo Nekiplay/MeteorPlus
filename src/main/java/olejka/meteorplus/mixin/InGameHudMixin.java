249ba2d2718db0e09820bb76f160f0038f60197f
bluepanee <53943505+bluepanee@users.noreply.github.com>
2023-06-21 20:07:25 +0500

    Update InGameHudMixin.java

--- src/main/java/olejka/meteorplus/mixin/InGameHudMixin.java
+++ src/main/java/olejka/meteorplus/mixin/InGameHudMixin.java
@@ -3,9 +3,7 @@ package olejka.meteorplus.mixin;
 import meteordevelopment.meteorclient.MeteorClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.ScoreboardObjective;
-import net.minecraft.text.Text;
 import olejka.meteorplus.events.ScoreBoardRenderEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
@@ -13,9 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-
-//	"Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I"
-	@Inject(method = "renderScoreboardSidebar", at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/ScoreboardObjective;getScoreboard()Lnet/minecraft/scoreboard/Scoreboard;", ordinal = 0))
+	@Inject(method = "renderScoreboardSidebar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 0))
 	private void renderScoreboardSidebar(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
 		MeteorClient.EVENT_BUS.post(ScoreBoardRenderEvent.get(context.getMatrices(), objective));
 	}
