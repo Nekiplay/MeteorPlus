13a5e12b2b139c1bc1a2b0a3d1ef8d809c85521d
Danilov Roman <35975332+Nekiplay@users.noreply.github.com>
2023-08-30 21:51:17 +0500

    Revert "typo fix"

--- src/main/java/nekiplay/meteorplus/features/modules/ItemHighlightPlus.java
+++ src/main/java/nekiplay/meteorplus/features/modules/ItemHighlightPlus.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class ItemHighlightPlus extends Module {
 	public ItemHighlightPlus() {
-		super(MeteorPlus.CATEGORY, "ItemHighlight+", "Highlights selected items when in inventorys and guis".");
+		super(MeteorPlus.CATEGORY, "ItemHighlight+", "Freezes your position for server.");
 	}
 
 	public final SettingGroup sgGeneral = settings.getDefaultGroup();
