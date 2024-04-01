dec8a5da93af06a079541bfec9f03629424cf8b8
Roman Danilov <neci.03@mail.ru>
2024-04-01 23:06:20 +0500

    Donate button

--- assets/js/github-api.js
+++ assets/js/github-api.js
@@ -34,3 +34,30 @@ window.onload = function(e){
         })
     } 
 }
+
+document.getElementById("clickMe").onclick = function () {
+	
+	let headers = new Headers();
+	
+	headers.append('Content-Type', 'application/json');
+	headers.append('Accept', 'application/json');
+	
+	headers.append('Access-Control-Allow-Origin', '*');
+	headers.append('Referer', 'https://meteor-plus.com/');
+	
+	headers.append('GET', 'POST', 'OPTIONS');
+
+
+	
+	fetch("https://lk.rukassa.is/api/v1/create?shop_id=2100&order_id=1&amount=1&token=907a67fe804b9b42a418828d597f5de5&currency=USD", {
+        method: "GET",
+		headers: headers
+    })
+	.then(x => x.json())
+    .then(data => 
+    {
+        var json = data
+		console.log(json)
+        window.open(json.url,"_self")
+    })
+};
