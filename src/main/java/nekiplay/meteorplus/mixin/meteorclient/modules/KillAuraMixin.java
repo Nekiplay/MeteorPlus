a6eec4e583ea5db0811d34696417c48c8f7bde09
Roman Danilov <neci.03@mail.ru>
2024-04-23 01:56:38 +0500

    Now has best killaura for PvE

--- src/main/java/nekiplay/meteorplus/mixin/meteorclient/modules/KillAuraMixin.java
+++ src/main/java/nekiplay/meteorplus/mixin/meteorclient/modules/KillAuraMixin.java
@@ -11,6 +11,7 @@ import meteordevelopment.meteorclient.systems.modules.combat.KillAura;
 import nekiplay.meteorplus.features.modules.combat.Teams;
 import nekiplay.meteorplus.features.modules.combat.criticals.CriticalsPlus;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import nekiplay.meteorplus.features.modules.combat.AntiBotPlus;
 import org.spongepowered.asm.mixin.Final;
@@ -21,17 +22,58 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Mixin(value = KillAura.class, remap = false, priority = 1001)
 public class KillAuraMixin extends Module {
 	@Final
 	@Shadow
 	private final SettingGroup sgTiming = settings.getGroup("Timing");
 
+	@Final
+	@Shadow
+	private final SettingGroup sgTargeting = settings.getGroup("Targeting");
+
+	@Final
+	@Shadow
+	private final Setting<Set<EntityType<?>>> entities = (Setting<Set<EntityType<?>>>) sgTargeting.get("entities");
+
+	@Final
+	@Shadow
+	private final List<Entity> targets = new ArrayList<>();
+
+	@Shadow
+
+	public Entity getTarget() {
+		if (!targets.isEmpty()) return targets.get(0);
+		return null;
+	}
+
 	@Unique
 	private final Setting<Boolean> onlyCrits = sgTiming.add(new BoolSetting.Builder()
 		.name("only-crits")
 		.description("Attack enemy only if this attack crit after jump.")
 		.defaultValue(true)
+		.build()
+	);
+
+	@Unique
+	private final Setting<Boolean> ignoreSmartDelayForShulkerBulletAndGhastCharge = sgTiming.add(new BoolSetting.Builder()
+		.name("ignore-delay-for-one-hit-entities")
+		.description("Ignore attack delay for shulker bullet and fireball.")
+		.defaultValue(true)
+		.visible(() -> entities.get().contains(EntityType.SHULKER_BULLET) || entities.get().contains(EntityType.FIREBALL))
+		.build()
+	);
+
+	@Unique
+	private final Setting<Boolean> ignoreOnlyCritsForOneHitEntity = sgTiming.add(new BoolSetting.Builder()
+		.name("ignore-only-crits-for-one-hit-entityies")
+		.description("Ignore only crits delay for shulker bullet and fireball.")
+		.defaultValue(true)
+		.visible(() -> ignoreSmartDelayForShulkerBulletAndGhastCharge.get() && onlyCrits.get())
 		.build()
 	);
 
@@ -44,7 +86,22 @@ public class KillAuraMixin extends Module {
 		if (onlyCrits.get() && !CriticalsPlus.allowCrit()) {
 			cir.setReturnValue(false);
 		}
+		else if (ignoreOnlyCritsForOneHitEntity.get() && oneHitEntity()) {
+			cir.setReturnValue(true);
+		}
+		else if (oneHitEntity() && ignoreSmartDelayForShulkerBulletAndGhastCharge.get() && !onlyCrits.get()) {
+			cir.setReturnValue(true);
+		}
 	}
+
+	@Unique
+	private boolean oneHitEntity() {
+		if (ignoreSmartDelayForShulkerBulletAndGhastCharge.get() && getTarget() != null && (getTarget().getType() == EntityType.FIREBALL || getTarget().getType() == EntityType.SHULKER_BULLET)) {
+			return true;
+		}
+		return false;
+	}
+
 
 	@Inject(method = "entityCheck", at = @At("RETURN"), cancellable = true)
 	protected void entityCheck(Entity entity, CallbackInfoReturnable<Boolean> cir) {
