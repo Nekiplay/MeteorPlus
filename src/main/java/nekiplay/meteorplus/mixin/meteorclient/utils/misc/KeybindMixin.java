c34869cf83d6e29a470535c4fe8e1d635e857af6
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-05-17 19:35:54 +0500

    Update KeybindMixin.java

--- src/main/java/nekiplay/meteorplus/mixin/meteorclient/utils/misc/KeybindMixin.java
+++ src/main/java/nekiplay/meteorplus/mixin/meteorclient/utils/misc/KeybindMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = meteordevelopment.meteorclient.utils.misc.Keybind.class, remap = false)
 public class KeybindMixin {
 	@Inject(method = "canBindTo", at = @At("HEAD"), cancellable = true)
-	public void canBind(boolean isKey, int value, CallbackInfoReturnable<Boolean> cir) {
+	public void canBind(boolean isKey, int value, int modifers, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(true);
 	}
 }
